9cb099b352a6d07687a246ab1d75942e4240fcb6
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-25 08:06:04 -0800

    Adding spring web app using spring initializer in the web

---
+++ 001_samplespringboot/samplespringboot/src/main/java/com/venkat/samplespringboot/SamplespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.venkat.samplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplespringbootApplication.class, args);
+	}
+
+}
