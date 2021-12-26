614b2c9cc88dc01357dd1acd48e8186c71e27104
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-26 00:17:02 -0800

    Rest api with exception handling - http status code

--- 002_samplespringbootMVC/samplespringbootmvc/src/main/java/com/venkat/samplespringbootmvc/web/StudentController.java
+++ 002_samplespringbootMVC/samplespringbootmvc/src/main/java/com/venkat/samplespringbootmvc/web/StudentController.java
@@ -2,9 +2,15 @@ package com.venkat.samplespringbootmvc.web;
 
 import com.venkat.samplespringbootmvc.entity.Student;
 import com.venkat.samplespringbootmvc.repository.StudentRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -23,5 +29,29 @@ public class StudentController {
             ans+= student.toString() +"..........................";
         }
         return ans;
+    }
+
+    @GetMapping("/api/students")
+    public List<Student> getAllStudentsInApi()
+    {
+        var students =  this.studentRepository.findAll();
+        List<Student> result = new ArrayList<Student>();
+        for (var student : students) {
+            result.add(student);
+        }
+
+        return result;
+    }
+
+    @GetMapping("/api/student/{id}")
+    public Student getAllStudentsInApi(@PathVariable("id") int id)
+    {
+        var student =  this.studentRepository.findById(id);
+       if(student.isPresent())
+       {
+           return student.get();
+       }else{
+           throw new ResponseStatusException(HttpStatus.BAD_GATEWAY, "Student with id "+id+" is not found.");
+       }
     }
 }
