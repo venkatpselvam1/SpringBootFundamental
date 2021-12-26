70b79ba55ae9be5ab57b5e845ca51798ac24492d
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-26 01:12:03 -0800

    Added unit test

---
+++ 002_samplespringbootMVC/samplespringbootmvc/src/test/java/com/venkat/samplespringbootmvc/StudentControllerUnitTest.java
@@ -0,0 +1,38 @@
+package com.venkat.samplespringbootmvc;
+
+import com.venkat.samplespringbootmvc.repository.StudentRepository;
+import com.venkat.samplespringbootmvc.web.StudentController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(StudentController.class)
+public class StudentControllerUnitTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @Test
+    public void getAllStudents() throws Exception
+    {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/students"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[]"));
+        verify(studentRepository, times(1)).findAll();
+    }
+}
